02e1009bd03d5e2bb1804849896c125e2aeea579
yingha <yinghalin@gmail.com>
2021-06-08 09:16:49 +0200

    add some tasks

--- Markov.py
+++ Markov.py
@@ -36,11 +36,15 @@ print(df['customer_no'].value_counts().sum())
 df.groupby('location').resample('H')['customer_no'].count()
 
 # %%
+# Display the number of customers at checkout over time
+
+# %%
 #Calculate the time each customer spent in the market
 df['timestamp'] = df.index
 time_spent =df.groupby(['weekday','customer_no'], as_index=False).agg({'timestamp':['min', 'max']})
 time_spent['spent'] = time_spent['timestamp', 'max'] - time_spent['timestamp', 'min']
 # extract integer (minutes)?
+
 # %%
 # Calculate the total number of customers in the supermarket over time.
 df2 = df.groupby(['weekday','customer_no'], as_index=False).resample('1Min').ffill()
@@ -58,11 +62,12 @@ vector_base['checkout'] = 0.0
 last_stations = df.groupby(['weekday','customer_no'], as_index=False).last()
 no_checkout = last_stations[last_stations['location']!='checkout']
 
-
-
 # %%
 row_filter = df['customer_no'].isin(no_checkout['customer_no']) & df['weekday'].isin(no_checkout['weekday'])
+
 # %%
+# Set an initial state distribution vector with all customers in the entrance
+# Calculate the next state as a dot product of your transition probability matrix P
 first_stations = df.groupby(['weekday','customer_no']).nth(0)['location']
 second_stations = df.groupby(['weekday','customer_no']).nth(1)['location']
 
@@ -73,6 +78,29 @@ new_df = pd.merge(first_stations, second_stations,
 
 new_df.fillna('checkout', inplace=True)
 
-
 P = pd.crosstab(new_df['location_y'], new_df['location_x'], normalize=True)
+
 # %%
+# Repeat from 2 for a number of steps
+# Plot the result
+
+# %%
+# Creating a state diagram with pygraphviz
+import pygraphviz as pgv
+states = ['dairy','drinks','fruit','spices','checkout']
+
+# init the graph
+G = pgv.AGraph(strict=False,directed=True)
+
+# loop over all pairs of states
+for state_from in states:
+    for state_to in states:
+        # get the transition probability
+        proba = P.loc[state_from, state_to]
+        # draw into the graph if the probability is larger zero
+        if proba > 0:
+            G.add_edge(state_from, state_to, label=np.round(proba, 2))
+
+# write the graph to hard drive            
+G.draw('transition.png', prog='dot')
+
